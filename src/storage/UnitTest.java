0a343a2fedd66a7cebc760206249b7b51592c2ba
Alexander Skjolden <alex@plastboks.net>
2015-02-19 14:58:02 +0100

    Simple insert test for Deque
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/storage/UnitTest.java
+++ src/storage/UnitTest.java
@@ -1,8 +1,36 @@
 package storage;
 
+import model.Boat;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by alex on 2/19/15.
  */
 public class UnitTest
 {
+    Deque<Boat> boats;
+
+    public UnitTest()
+    {
+        boats = new Deque<Boat>();
+    }
+
+    @Test
+    public void test_insert()
+    {
+        String[] regNrs = {"A123", "A234", "A456",
+                           "B123", "B234", "B456",
+                           "C123", "C234", "C456"};
+
+        for (int i = 0; i < regNrs.length; i++) {
+            boats.addLast(new Boat.Builder(regNrs[i], "Sailboat").build());
+        }
+
+        for (int i = 0; i < regNrs.length; i++) {
+            Boat pop = boats.removeFirst();
+            assertTrue(pop.getRegnr().equals(regNrs[i]));
+            assertTrue(pop.getType().equals("Sailboat"));
+        }
+    }
 }
